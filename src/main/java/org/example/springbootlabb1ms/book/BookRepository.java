9ec38f81f602df6e2fe06140ac9bb8029dba8c8d
Martin Stenhagen <martin.stenhagen@iths.se>
2026-03-09 09:29:46 +0100

    Added interface BookRepository

---
+++ src/main/java/org/example/springbootlabb1ms/book/BookRepository.java
@@ -0,0 +1,14 @@
+package org.example.springbootlabb1ms.book;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends ListCrudRepository<Book, Long> {
+
+    List<Book> findByTitleContainingIgnoreCase(String title);
+
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+
+    boolean existsByIsbn(String isbn);
+}
